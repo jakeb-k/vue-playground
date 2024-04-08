a07be9bbc0d9a3de2171c8e8708ba647e0acfa86
jakeb-k <jknow30@outlook.com>
2024-04-08 15:51:36 +1000

    custom fly in anims

--- resources/js/Components/Profile.vue
+++ resources/js/Components/Profile.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="h-3/5" >
+    <div class="h-3/5 animate-fly-in-from-right" >
       <img src="../assets/images/profile.webp" alt="profile" class="h-[400px] w-[400px] border-[3px] border-gray-800 rounded-full object-cover">
     </div>
 </template>
