70416549320c898f58a693ac4a0f0f4a2115ac2f
jakeb-k <jknow30@outlook.com>
2024-04-08 11:58:21 +1000

    nav layout

--- resources/js/Layouts/MasterLayout.vue
+++ resources/js/Layouts/MasterLayout.vue
@@ -6,14 +6,23 @@ import Logo from '@/Components/Logo.vue';
 
 </script>
 <template>
-    <nav class="bg-white border-b border-gray-100 py-2 shadow-lg">
-                <!-- Primary Navigation Menu -->
-        <Link href="/#about"> About </Link>  
-        <Link> Projects</Link>    
-        <Logo></Logo>
-        <Link> Contact </Link>    
-        <Link> Repositories </Link>    
+    <nav class="bg-white border-b border-black py-2 shadow-lg ">
+        <!-- Primary Navigation Menu -->
+    <div class="flex justify-center w-full">
 
+    
+        <div class="w-1/3 flex justify-around">
+            <Link href="/#about"> About </Link>  
+            <Link> Projects</Link> 
+        </div>
+        <div class="w-1/3 flex justify-around">
+            <Logo></Logo>
+        </div>
+        <div class="w-1/3 flex justify-around">
+            <Link> Contact </Link>    
+            <Link> Repositories </Link>    
+        </div>   
+    </div>
     </nav>
     <!-- Page Content -->
     <main>
