0a3e36454628e418099615eba60183ccbed07f6b
jakeb-k <jknow30@outlook.com>
2024-04-09 14:07:37 +1000

    contact sec styled

--- resources/js/Components/ContactForm.vue
+++ resources/js/Components/ContactForm.vue
@@ -6,17 +6,17 @@
         <form class="flex flex-col w-[500px] mt-20 mx-auto border-[3px] border-neon p-8
         bg-night rounded-lg" method="POST" action='{{url("/contact")}}' id="contact">
         
-        <h1 class="text-4xl text-center font-bebas text-neon tracking-wider">Get In Touch</h1>
+        <h1 class="text-5xl text-center font-bebas text-neon tracking-wider">Get In Touch</h1>
         
-        <label for="name">Name:</label>
-        <input type="text" name="name" id="name" />
+        <label class="text-2xl text-neon font-mono mb-2" for="name">Name:</label>
+        <input class="mb-8 focus:border-ocean" type="text" name="name" id="name" />
 
 
-        <label for="email">Email:</label>
-        <input type="text" name="email" id="email" />
+        <label class="text-2xl text-neon font-mono mb-2" for="email">Email:</label>
+        <input class="mb-8 focus:border-ocean" type="text" name="email" id="email" />
 
-        <label for="note">Message:</label>
-        <textarea name="note" id="note"></textarea>
+        <label class="text-2xl text-neon font-mono mb-2" for="note">Message:</label>
+        <textarea class="mb-8 focus:border-ocean h-44" name="note" id="note"></textarea>
  
         <!-- <div class="captcha">
             {!! NoCaptcha::display() !!}
@@ -27,7 +27,8 @@
             @endif
         </div>  -->
 
-        <button type="submit">Send Message!</button>
+        <button class="border-4 border-neon shadow-about rounded-xl py-4 w-full text-neon text-4xl font-bebas
+        tracking-wider duration-300 ease-in-out cursor-pointer hover:bg-neon hover:text-night" type="submit">Send Message!</button>
     </form>
     </div>
 </template>
